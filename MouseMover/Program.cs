b367ba4b67f50ee289d32d0ddd57564b03c296f9
Nilton Nascimento <nilton.nascimento@sovos.com>
2020-12-21 19:08:47 -0300

    Adding project to github

---
+++ MouseMover/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace MouseMover
+{
+    class Program
+    {
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(out Point lpPoint);
+
+        static void Main(string[] args)
+        {
+            MoveCursor();
+        }
+
+        private static void MoveCursor()
+        {
+            bool left = true;
+            Point point;
+
+            while(true)
+            {
+                GetCursorPos(out point);
+
+                if (left)
+                    SetCursorPos(point.X - 1, point.Y);
+                else
+                    SetCursorPos(point.X + 1, point.Y);
+
+                left = !left;
+
+                Console.WriteLine("Mouse Moved!");
+                Thread.Sleep(2 * 60000);
+            }
+            
+        }
+    }
+}
